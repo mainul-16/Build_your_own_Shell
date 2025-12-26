94e23a0f5d453544cd089ab04bc4cf19bf1c386d
mainul-16 <mainulislam160105@gmail.com>
2025-12-26 21:11:58 +0530

    [Hi]

--- app/main.go
+++ app/main.go
@@ -9,12 +9,15 @@ import (
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
+	// Print shell prompt
+	fmt.Print("$ ")
+
 	// Read user input
 	command, err := reader.ReadString('\n')
 	if err != nil {
 		return
 	}
 
-	// Remove trailing newline and print error message
+	// Remove newline and print error
 	fmt.Println(command[:len(command)-1] + ": command not found")
 }

2d5a00489a6e5cdd0c4108019d3eb9b21001ef4b
mainul-16 <mainulislam160105@gmail.com>
2025-12-27 21:20:16 +0530

    Fix echo builtin (CRLF-safe, prefix-based)

--- your_program.sh
+++ your_program.sh
@@ -4,18 +4,23 @@ while true; do
   printf "$ "
   read -r line || exit 0
 
+  # Remove carriage return if present (CRLF-safe)
+  line=$(printf "%s" "$line" | tr -d '\r')
+
   # exit builtin
   if [ "$line" = "exit" ]; then
     exit 0
   fi
 
-  # echo builtin (strip leading "echo ")
+  # echo builtin (match anything starting with "echo")
   case "$line" in
     echo\ *)
       printf "%s\n" "${line#echo }"
       ;;
+    echo)
+      printf "\n"
+      ;;
     *)
-      # ignore empty line
       [ -z "$line" ] && continue
       printf "%s: command not found\n" "$line"
       ;;

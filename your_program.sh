9eac271c275f028de5ee6ad24e25df430b4cb6f7
mainul-16 <mainulislam160105@gmail.com>
2025-12-27 21:21:27 +0530

    Fix echo builtin (CRLF-safe, prefix-based)

--- your_program.sh
+++ your_program.sh
@@ -4,25 +4,22 @@ while true; do
   printf "$ "
   read -r line || exit 0
 
-  # Remove carriage return if present (CRLF-safe)
-  line=$(printf "%s" "$line" | tr -d '\r')
-
   # exit builtin
   if [ "$line" = "exit" ]; then
     exit 0
   fi
 
-  # echo builtin (match anything starting with "echo")
+  # echo builtin: print everything after the first space
   case "$line" in
     echo\ *)
-      printf "%s\n" "${line#echo }"
+      printf "%s\n" "${line#* }"
       ;;
     echo)
       printf "\n"
       ;;
     *)
       [ -z "$line" ] && continue
-      printf "%s: command not found\n" "$line"
+      printf "%s: command not found\n" "${line%% *}"
       ;;
   esac
 done

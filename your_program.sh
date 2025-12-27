e93d579a7443f10390395dd1abbf3846d4ccf771
mainul-16 <mainulislam160105@gmail.com>
2025-12-27 20:46:49 +0530

    Fix echo builtin parsing (robust)

--- your_program.sh
+++ your_program.sh
@@ -2,15 +2,24 @@
 
 while true; do
   printf "$ "
-  read -r cmd args || exit 0
+  read -r line || exit 0
+
+  # Skip empty lines
+  [ -z "$line" ] && continue
+
+  # Split input safely into an array
+  read -r -a tokens <<< "$line"
+
+  cmd="${tokens[0]}"
+  args=("${tokens[@]:1}")
 
   if [ "$cmd" = "exit" ]; then
     exit 0
 
   elif [ "$cmd" = "echo" ]; then
-    printf "%s\n" "$args"
+    printf "%s\n" "${args[*]}"
 
-  elif [ -n "$cmd" ]; then
+  else
     printf "%s: command not found\n" "$cmd"
   fi
 done

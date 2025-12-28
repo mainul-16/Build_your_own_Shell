1f0ea22fafb91526f48007b6bf7182599b780701
mainul-16 <mainulislam160105@gmail.com>
2025-12-28 18:28:56 +0530

    Run external programs with arguments

--- app/main.go
+++ app/main.go
@@ -1,124 +1,108 @@
-package shell
+package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
-
-	"github.com/chzyer/readline"
-	"github.com/codecrafters-io/shell-starter-go/internal/handlers"
-	"github.com/codecrafters-io/shell-starter-go/internal/parser"
-	"github.com/codecrafters-io/shell-starter-go/internal/router"
+	"syscall"
 )
 
-func ListenAndServe(r router.Router) error {
-	completer := readline.NewPrefixCompleter(
-		readline.PcItem("echo"),
-		readline.PcItem("exit"),
-		readline.PcItem("cd"),
-		readline.PcItem("pwd"),
-		readline.PcItem("type"),
-	)
-	rl, _ := readline.NewEx(&readline.Config{
-		Prompt:       "$ ",
-		AutoComplete: completer,
-	})
-	for {
-		command, err := rl.Readline()
-		if err != nil {
-			return errors.New(fmt.Sprintf("Error reading input: %v\n", err))
-		}
+/* -------- RAW MODE (stdlib only) -------- */
 
-		op, parseinfo := parser.Parse(command)
+func enableRawMode() func() {
+	fd := int(os.Stdin.Fd())
 
-		if op == "" {
-			continue
-		}
-
-		out, err := r.Run(op, parseinfo.Arguments)
-
-		if errors.Is(err, handlers.ErrShellExit) {
-			return err
-		}
-
-		if errors.Is(err, handlers.ErrNoSuchFileOrDirectory) {
-			writeError(err, parseinfo)
-			continue
-		}
-
-		if errors.Is(err, handlers.ErrCommandNotFound) {
-			writeError(err, parseinfo)
-			continue
-		}
-
-		if errors.Is(err, handlers.ErrNotFound) {
-			writeError(err, parseinfo)
-			continue
-		}
-
-		/*
-			if no error but error output file provided
-			should create empty file
-		*/
-		writeLine(out, parseinfo)
-		writeError(err, parseinfo)
-	}
-}
-
-func writeLine(s string, info parser.ParsedInfo) {
-	if s == "" && info.OutputRedirectsRest == nil && info.OutputRedirectsNew == nil {
-		return
-	}
-
-	if info.OutputRedirectsRest == nil && info.OutputRedirectsNew == nil {
-		fmt.Fprintln(os.Stdout, strings.TrimRight(s, "\n"))
-		return
-	}
-
-	for _, outPath := range info.OutputRedirectsNew {
-		file, _ := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-		if s != "" {
-			fmt.Fprintln(file, strings.TrimRight(s, "\n"))
-		}
-		_ = file.Close()
-	}
-
-	for _, outPath := range info.OutputRedirectsRest {
-		file, _ := os.OpenFile(outPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-
-		if s != "" {
-			fmt.Fprintln(file, strings.TrimRight(s, "\n"))
-		}
-		_ = file.Close()
-	}
-}
-
-func writeError(err error, info parser.ParsedInfo) {
-	var msg string
-
+	oldState, err := syscall.IoctlGetTermios(fd, syscall.TCGETS)
 	if err != nil {
-		msg = err.Error()
+		return func() {}
 	}
 
-	for _, errPath := range info.ErrRedirectNew {
-		file, _ := os.OpenFile(errPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-		if msg != "" {
-			fmt.Fprintln(file, strings.TrimRight(msg, "\n"))
+	newState := *oldState
+	newState.Lflag &^= syscall.ICANON | syscall.ECHO
+	newState.Cc[syscall.VMIN] = 1
+	newState.Cc[syscall.VTIME] = 0
+
+	_ = syscall.IoctlSetTermios(fd, syscall.TCSETS, &newState)
+
+	return func() {
+		_ = syscall.IoctlSetTermios(fd, syscall.TCSETS, oldState)
+	}
+}
+
+/* -------- AUTOCOMPLETE -------- */
+
+func autocomplete(s string) (string, bool) {
+	if strings.HasPrefix("echo", s) {
+		return "echo ", true
+	}
+	if strings.HasPrefix("exit", s) {
+		return "exit ", true
+	}
+	return s, false
+}
+
+/* -------- MAIN -------- */
+
+func main() {
+	restore := enableRawMode()
+	defer restore()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("$ ")
+
+		var buf strings.Builder
+
+		for {
+			ch, err := reader.ReadByte()
+			if err != nil {
+				if err == io.EOF {
+					fmt.Println()
+					return
+				}
+				continue
+			}
+
+			// ENTER
+			if ch == '\n' {
+				fmt.Println()
+				break
+			}
+
+			// TAB
+			if ch == '\t' {
+				text := buf.String()
+				if completed, ok := autocomplete(text); ok {
+					fmt.Print("\r$ ")
+					fmt.Print(completed)
+					buf.Reset()
+					buf.WriteString(completed)
+				}
+				continue
+			}
+
+			// NORMAL CHAR
+			buf.WriteByte(ch)
+			fmt.Printf("%c", ch)
 		}
-		_ = file.Close()
-	}
 
-	for _, errPath := range info.ErrRedirectRest {
-		file, _ := os.OpenFile(errPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-		if msg != "" {
-			fmt.Fprintln(file, strings.TrimRight(msg, "\n"))
+		line := strings.TrimSpace(buf.String())
+		if line == "" {
+			continue
 		}
 
-		_ = file.Close()
-	}
+		if line == "exit" {
+			return
+		}
 
-	if msg != "" && info.ErrRedirectRest == nil && info.ErrRedirectNew == nil {
-		fmt.Fprintln(os.Stdout, strings.TrimRight(msg, "\n"))
+		if strings.HasPrefix(line, "echo ") {
+			fmt.Println(strings.TrimPrefix(line, "echo "))
+			continue
+		}
+
+		fmt.Printf("%s: command not found\n", line)
 	}
 }
